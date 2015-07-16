187263d9813887085a536cd82c5817615ccd2cfc
ryeyuen <ryeyuen@gmail.com>
2015-07-16 17:30:18 -0600

    Inital Checkin

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public int TheaterId { get; set; }
+        
+        [StringLength(10)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual Theater Theater {get; set;}
+    }
+ 
+    public class Theater
+    {
+        public int TheaterId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MvcMovie/Models/MyTestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+namespace MvcMovie.Models
+{
+    public class MyTestClass
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class NiggaDbContext : DbContext
+    {
+        public DbSet<MyTestClass> MyTestClasses { get; set; }
+    }
+}
